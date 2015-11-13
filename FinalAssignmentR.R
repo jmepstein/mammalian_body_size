c13f7ef26223e64ca1f252a1e9b5d9055ec34f59
Joshua Epstein <jmepstein12@ufl.edu>
2015-11-13 14:53:12 -0500

    Calculating the means among different continents. Creating a data frame with continent and avergae mass of extant and extinct species for each continent. Exporting data frame into a CSV file.

--- FinalAssignmentR.R
+++ FinalAssignmentR.R
@@ -14,7 +14,24 @@ colnames(mammal_sizes) <- c("continent", "status", "order",
                             "family", "genus", "species", "log_mass", "combined_mass", 
                             "reference")
 head(mammal_sizes)
+mammal_sizes$continent
 
 #Calculating the mean mass of the extinct species and the extant spcies:
 mean_mass_extinct <- mean(mammal_sizes$combined_mass[mammal_sizes$status == "extinct"], na.rm = T)
 mean_mass_extant <- mean(mammal_sizes$combined_mass[mammal_sizes$status == "extant"], na.rm = T)
+
+#Calculating the mean mass of extinct species among different continents:
+extinct_mass <- mammal_sizes %>%
+                filter(status == 'extinct')%>%
+                group_by(continent) %>%
+                summarize(mean(combined_mass))
+
+#Calculating the mean mass of extant species among different continents:
+extant_mass <- mammal_sizes %>%
+              filter(status == 'extant')%>%
+              group_by(continent) %>%
+              summarize(mean(combined_mass, na.rm=T))
+  
+  
+  
+  
